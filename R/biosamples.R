ae9017e1fcbcc0387692c00289a181e5491e3874
Jesse Connell <ancon@upenn.edu>
2018-11-19 12:03:06 -0500

    reorganize NCBI metadata download/read functions

--- R/biosamples.R
+++ R/biosamples.R
@@ -87,6 +87,9 @@ write_biosamples <- function(data, ...) {
 
 # BioSample Package / Template Info ---------------------------------------
 
+# These functions are structured in pairs of download/read steps, so we can
+# keep a raw copy of the metadata inside the package and update from NCBI when
+# needed.
 
 #' Download SRA BioSample Metadata
 #'
@@ -96,7 +99,7 @@ write_biosamples <- function(data, ...) {
 #' @param dp directory path to save files to.  If not specified, the extdata
 #'   subdirectory of the package location will be used.
 #'
-#' @return data frame of information for all available BioSample types.
+#' @return named vector of downloaded file paths.
 #'
 #' @export
 dump_metadata <- function(dp=NULL) {
@@ -106,71 +109,182 @@ dump_metadata <- function(dp=NULL) {
   if (! dir.exists(dp)) {
     dir.create(dp, recursive = TRUE)
   }
-  # Download BioSample Package data
-  biosample_packages <- download_biosample_packages()
-  write_sra_table(biosample_packages,
-                  fp = file.path(dp, "biosample_packages.tsv"))
-  dp_templates <- file.path(dp, "templates", "biosample_attributes")
-  if (! dir.exists(dp_templates)) {
-    dir.create(dp_templates, recursive = TRUE)
-  }
-  # Download BioSample Template files
-  templates <- lapply(biosample_packages$Name, download_template)
-  names(templates) <- biosample_packages$Name
-  for (tn in biosample_packages$Name) {
-    fp <- file.path(dp_templates, paste0(tn, ".tsv"))
-    writeChar(templates[[tn]], fp, eos=NULL)
-  }
+  fps <- download_biosample_packages()
+  data <- read_biosample_packages(fps)
+  fps <- c(fps, sapply(data$Name, download_template))
+  fps
 }
 
-
-#' Download BioSample Package Information
+#' Download BioSample Package info XML
 #'
-#' Download an XML file of BioSample Package information (including template
+#' Download an XML file of BioSample package information and write to disk.
+#'
+#' @param fp file path to write to, or NULL to use a default within the package
+#'   directory.
+#'
+#' @return file path written to
+#'
+#' @export
+download_biosample_packages <- function(fp=NULL) {
+  # (Or one at a time by giving a path after the last "/" of a package name.)
+  url_xml <- paste0("https://",
+                    HTTP_SRV["WWW"],
+                    "/biosample/docs/packages/?format=xml")
+  if (is.null(fp)) {
+    dp <- system.file("extdata", package = methods::getPackageName())
+    fp <- file.path(dp, "biosample_packages.xml")
+  }
+  utils::download.file(url = url_xml, destfile = fp)
+  c(biosample_packages = fp)
+}
+
+#' Read BioSample package information from file
+#'
+#' Read in XML file of BioSample package information (including template
 #' names) and return a data frame.
+#'
+#' @param fp file path to write to, or NULL to use a default within the package
+#'   directory.
 #'
 #' @return data frame of information for all available BioSample types.
 #'
 #' @export
-download_biosample_packages <- function() {
-  url_xml <- paste0("https://",
-                    HTTP_SRV["WWW"],
-                    "/biosample/docs/packages/?format=xml")
-  x <- xml2::xml_children(xml2::read_xml(url_xml))
+read_biosample_packages <- function(fp=NULL) {
+  if (is.null(fp)) {
+    dp <- system.file("extdata", package = methods::getPackageName())
+    fp <- file.path(dp, "biosample_packages.xml")
+  }
+  data <- xml2::xml_children(xml2::read_xml(fp))
   columns <- c("Name", "DisplayName", "ShortName", "EnvPackage",
                "EnvPackageDisplay", "Description", "Example")
   names(columns) <- columns
-  data <- lapply(columns, function(column) xml2::xml_text(xml2::xml_child(x, column)))
+  data <- lapply(columns, function(column) {
+    xml2::xml_text(xml2::xml_child(data, column))
+  })
   data <- do.call(cbind.data.frame, c(data,list(stringsAsFactors = FALSE)))
   data
 }
 
+#' Download BioSample Attributes XML
+#'
+#' Download an XML file of BioSample field attribute information and write to
+#' disk.
+#'
+#' @param fp file path to write to, or NULL to use a default within the package
+#'   directory.
+#'
+#' @return file path written to
+#'
+#' @export
+download_biosample_attributes <- function(fp=NULL) {
+  url_xml <- paste0("https://",
+                    HTTP_SRV["WWW"],
+                    "/biosample/docs/attributes/?format=xml")
+  if (is.null(fp)) {
+    dp <- system.file("extdata", package = methods::getPackageName())
+    fp <- file.path(dp, "biosample_package_attributes.xml")
+  }
+  utils::download.file(url = url_xml, destfile = fp)
+  c(biosample_package_attributes = fp)
+}
+
+#' Read BioSample attributes list from file
+#'
+#' Read in an XML file of BioSample field attributes and return as nested list
+#' structure.  (This doesn't fit cleanly into a table like the packages
+#' information does.)
+#'
+#' @param fp file path to read from, or NULL to use a default within the package
+#'   directory.
+#'
+#' @return nested list of BioSample field attributes.  Each named entry refers
+#'   to a single field (column) used by one or more package templates.  Each
+#'   entry contains "metadata" and "extra"; metadata stores basic information
+#'   like a description of the field and what packages it's used in.  extra
+#'   refers to additional per-metadata-item characteristics (currently only used
+#'   for Packages).
+#'
+#' @export
+read_biosample_attributes <- function(fp=NULL) {
+  if (is.null(fp)) {
+    dp <- system.file("extdata", package = methods::getPackageName())
+    fp <- file.path(dp, "biosample_package_attributes.xml")
+  }
+  data <- xml2::xml_children(xml2::read_xml(fp))
+  output <- lapply(data, function(entry) {
+    children <- xml2::xml_children(entry)
+    entry_names <- xml2::xml_name(children)
+    entry_contents <- split(xml2::xml_text(children), entry_names)
+    entry_attrs <- split(xml2::xml_attrs(children), entry_names)
+    list(metadata=entry_contents, extra=entry_attrs)
+  })
+  names(output) <- sapply(output, function(x) x$metadata$HarmonizedName)
+  output
+}
+
 #' Download BioSample Template
 #'
-#' Download a BioSample template as a TSV file and return the text.
+#' Download a BioSample template as a TSV file.
 #'
 #' @param package_name name of BioSample Package for the template to download,
 #'   (e.g., "MIMS.me.human-associated.4.0")
+#' @param fp file path to write to, or NULL to use a default within the package
+#'   directory.
 #'
-#' @return text of TSV template
+#' @return file path written to
 #'
 #' @export
-download_template <- function(package_name) {
+download_template <- function(package_name, fp=NULL) {
+  # read_template is currently in templates.R.  Should tidy that in one
+  # direction or the other.
   args <- c(package=package_name, action="download_tsv") # or download_excel
   url_args <- paste(names(args), args,
                     sep = "=",
                     collapse = "&")
   url <- paste0("https://", HTTP_SRV["SUBMIT"], "/biosample/template/?", url_args)
-  con <- file(url, "r")
-  txt <- ""
-  N <- 1024*1024
-  chunk <- readChar(con, N)
-  while (length(chunk) > 0) {
-    txt <- paste(txt, chunk)
-    chunk <- readChar(con, N)
+  if (is.null(fp)) {
+    dp <- system.file("extdata", package = methods::getPackageName())
+    dp_templates <- file.path(dp, "templates", "biosample_attributes")
+    if (! dir.exists(dp_templates)) {
+      dir.create(dp_templates, recursive = TRUE)
+    }
+    fp <- file.path(dp_templates, paste0(package_name, ".tsv"))
   }
-  close(con)
-  # Remove leading space
-  txt <- sub("^ +", "", txt)
-  txt
+  utils::download.file(url = url, destfile = fp)
+  names(fp) <- package_name
+  return(fp)
+}
+
+# Other -------------------------------------------------------------------
+
+
+#' Get BioSample field descriptions
+#'
+#' Given a vector of BioSample field (column) names, return SRA descriptions for
+#' each.
+#'
+#' @param fields vector of field names, like \code{c("env_biome", "env_feature")}.
+#'
+#' @return named vector of descriptions, named by the fields given.
+#'
+#' @export
+field_descriptions <- function(fields) {
+  bs_attrs <- read_biosample_attributes()
+  has_name <- function(field, attrib) {
+    (field == attrib$metadata$HarmonizedName) ||
+      (field %in% unlist(attrib$metadata$Synonym))
+  }
+
+  desc <- sapply(fields, function(field) {
+    for (attrib in bs_attrs) {
+      if (has_name(field, attrib))
+        return(attrib$metadata$Description)
+    }
+  })
+
+  names(desc) <- fields
+  # Trim off whitespace including trailing line endings
+  # https://stackoverflow.com/a/2261149
+  trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+  trim(desc)
 }
